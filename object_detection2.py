fb81e67fd737514c6ebe9e05add7f017b69d842b
kant003 <gonzalezm.angel@gmail.com>
2019-02-28 12:25:50 +0100

    aa

--- object_detection2.py
+++ object_detection2.py
@@ -43,8 +43,8 @@ GPIO.setup(18,GPIO.OUT)
 # Set up camera constants
 #IM_WIDTH = 1280
 #IM_HEIGHT = 720
-IM_WIDTH = 640    Use smaller resolution for
-IM_HEIGHT = 480   slightly faster framerate
+IM_WIDTH = 640    #Use smaller resolution for
+IM_HEIGHT = 480   #slightly faster framerate
 
 # Select camera type (if user enters --usbcam when calling this script,
 # a USB webcam will be used)
